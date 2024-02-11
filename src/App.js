fa0619baa48fed184780888832ec487e5366e13e
Dylan <dylan.coben@gmail.com>
2024-02-10 19:43:17 -0500

    Added background and some audio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 import confetti from 'canvas-confetti';
+import heartBackground from './images/hearts.jpg';
+import vineBoom from './audio/vine_boom.mp3';
+import yippee from './audio/yippee.mp3';
+import ahh from './audio/ahh.mp3';
+
 
 const noPhrases = ['no', 'pleaseee', 'I hate puppies', 'I am picking the wrong choice', 'whyyyyyyy', 'This is your LAST CHANCE', 'FR FR you better not do it this time']
 const yesPhrases = ['yes', 'YES', 'YES!!!', 'PICK THIS ONE!!!', 'why u no click me :(', 'yes 🥺', "OF COURSE!!!"]
@@ -9,6 +14,10 @@ function App() {
 
   // Called when the "No" button is clicked
   const onNoClick = () => {
+    const audioFiles = [vineBoom, ahh];
+    const randomAudioIndex = Math.floor(Math.random() * audioFiles.length);
+    new Audio(audioFiles[randomAudioIndex]).play();
+    
     handleMouseOver();
     setPhraseIndex(phraseIndex + 1);
     setNoButtonStyle({
@@ -18,58 +27,64 @@ function App() {
   }
 
   const [phraseIndex, setPhraseIndex] = useState(0);
-  const [noButtonStyle, setNoButtonStyle] = useState({});
+  const [noButtonStyle, setNoButtonStyle] = useState({fontSize: `${20 - 2 * phraseIndex}px`});
+  const [yesButtonPressed, setYesButtonPressed] = useState(false);
+
   const [showNo, setShowNo] = useState(true);
   const [imageSrc, setImageSrc] = useState('https://media1.tenor.com/m/HzAhXPf2YOEAAAAC/milk-and-mocha.gif');
 
 
   // Moves the button when when mouseover
-  const handleMouseOver = (e) => {
+    const handleMouseOver = (e) => {
 
-    //if (phraseIndex <= 1 || phraseIndex === noPhrases.length - 1) return;
-    if (phraseIndex <= 1) return;
+      //if (phraseIndex <= 1 || phraseIndex === noPhrases.length - 1) return;
+      if (phraseIndex <= 1) return;
 
-    // generates a random value between min and max, ensuring that the value is at least minAbs
-    const generateRandom = (min, max, minAbs) => {
-      let num = Math.random() * (max - min) + min;
+      // generates a random value between min and max, ensuring that the value is at least minAbs
+      const generateRandom = (min, max, minAbs) => {
+        let num = Math.random() * (max - min) + min;
 
-      // Ensure that the abs value is at least minAbs
-      if (Math.abs(num) < minAbs) {
-        num = num < 0 ? minAbs : minAbs;
+        // Ensure that the abs value is at least minAbs
+        if (Math.abs(num) < minAbs) {
+          num = num < 0 ? minAbs : minAbs;
+        }
+        return num;
       }
-      return num;
-    }
 
-  // Generate random positions within a range for the button to move to
-  const moveX = generateRandom(-100, 100, (phraseIndex * 10) + 20);
-  const moveY = generateRandom(-100, 100, (phraseIndex * 10) + 20);
-  // Set the button to move to the new position
-  setNoButtonStyle({
-    ...noButtonStyle,
-    transform: `translate(${moveX}px, ${moveY}px)`,
-    transition: 'transform 0.3s ease-in-out',
+    // Generate random positions within a range for the button to move to
+    const moveX = generateRandom(-100, 100, (phraseIndex * 10) + 20);
+    const moveY = generateRandom(-100, 100, (phraseIndex * 10) + 20);
+    // Set the button to move to the new position
+    setNoButtonStyle({
+      ...noButtonStyle,
+      transform: `translate(${moveX}px, ${moveY}px)`,
+      transition: 'transform 0.3s ease-in-out',
 
-  });
-}
+    });
+  }
 
-const onYesClick = () => {
-  launchConfetti();
-  setShowNo(false);
-  setImageSrc('https://media1.tenor.com/m/wbntPv9hoXoAAAAC/cuddle-panda.gif')
-}
-const launchConfetti = () => {
-  
-  confetti({
-    particleCount: 200,
-    spread: 90,
-    origin: { y: 0.6 }
-  });
-  
-}
+  const onYesClick = () => {
+    new Audio(yippee).play();
+    setYesButtonPressed(true);
+    launchConfetti();
+    setShowNo(false);
+    setImageSrc('https://media1.tenor.com/m/wbntPv9hoXoAAAAC/cuddle-panda.gif')
+  }
+  const launchConfetti = () => {
+    
+    confetti({
+      particleCount: 200,
+      spread: 90,
+      origin: { y: 0.6 }
+    });
+    
+  }
 
   return (
-    <main className="w-screen h-screen overflow-hidden flex justify-center items-center bg-white-100">
+    <main className="w-screen h-screen overflow-hidden flex justify-center items-center bg-white-100"
+    style={{ backgroundImage: `${yesButtonPressed ? `url(${heartBackground})` : 'none'}` }}>
       {/* This div centers the content horizontally */}
+     
       <div>
         <div className="flex justify-center pb-10">
           <img 
@@ -79,19 +94,26 @@ const launchConfetti = () => {
         </div>
         
         <div className="text-center">
-
-          <h1 className="text-3xl font-bold">
-            Will you be my valentine?
-          </h1>
+            <div className="rounded-full bg-white">
+            <h1 className="text-3xl font-bold rounded-xl bg-red-300 px-5 py-4">
+              <div>
+                {yesButtonPressed ? "I love you!!💖💖💖" : "Will you be my valentine?"}
+              </div>
+            </h1>
+          </div>
 
           <div class="flex justify-center space-x-4 mt-8">
+            {/*Yes button*/}
+            {/*Sets the button size to increase over time or be 100 if yesButton is pressed*/}
             <button
+            
             className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-700 transition duration 300"
-            style={{ fontSize: `${(phraseIndex + 1) * 15}px`}}
+            style={{ fontSize: `${( yesButtonPressed ? 150 : (phraseIndex + 1.5) * 15)}px`}}
             onClick={onYesClick}>
-              {yesPhrases[phraseIndex % (yesPhrases.length)]}
+              {yesButtonPressed ? "YIPPEE" : yesPhrases[phraseIndex % (yesPhrases.length)]}
             </button>
 
+            {/*No button*/}
             {showNo && <div className="flex justify-center items-center text-center">
               <button
               onClick={onNoClick}
